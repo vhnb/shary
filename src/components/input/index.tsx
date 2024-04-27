a9163287443471b18de87c978687d88cb71f8931
Victor Henrique <123006884+vhnb@users.noreply.github.com>
2024-04-27 14:58:14 -0300

    Delete src directory

--- src/components/input/index.tsx
+++
@@ -1,6 +0,0 @@
-import { HTMLProps } from 'react'
-import styles from './styles.module.css'
-
-export function Input({...rest}: HTMLProps<HTMLInputElement>){
-    return <input className={styles.input} {...rest}></input>
-}
