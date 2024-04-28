2d0fa107b741a0ba0254d5f7943424918859a205
Victor Henrique <123006884+vhnb@users.noreply.github.com>
2024-04-28 11:39:02 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="https://i.ibb.co/N7bbX9g/Captura-de-tela-2024-04-28-113437.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
